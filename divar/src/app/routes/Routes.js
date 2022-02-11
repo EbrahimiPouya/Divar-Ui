3338ebe6a5a2745cfd4a861163613514c909be00
pouya <p.ebrahimi163@gmail.com>
2022-02-11 21:36:34 +0330

    change directories add routes

---
+++ divar/src/app/routes/Routes.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import UserLogList from "./divar/sections/log/UserLogList";
+
+class Routes extends Component {
+    render() {
+        return (
+            <div>
+                <UserLogList/>
+            </div>
+        );
+    }
+}
+
+export default Routes;
